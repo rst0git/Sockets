8f7754643c41f77867ca618d70637be8f5421517
Radostin Stoyanov <rstoyanov1@gmail.com>
2018-08-28 14:44:30 +0100

    Add pipe example

---
+++ PIPE/pipe.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int pipefd[2];
+    pid_t cpid;
+    char buf;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage %s <string>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    cpid = fork();
+    if (cpid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (cpid == 0) {
+        close(pipefd[1]);
+
+        while (read(pipefd[0], &buf, 1))
+            write(STDOUT_FILENO, &buf, 1);
+
+        write(STDOUT_FILENO, "\n", 1);
+        close(pipefd[0]);
+        _exit(EXIT_SUCCESS);
+    } else {
+        close(pipefd[0]);
+        write(pipefd[1], argv[1], strlen(argv[1]));
+        close(pipefd[1]);
+        wait(NULL);
+        exit(EXIT_FAILURE);
+    }
+    return 0;
+}
+
