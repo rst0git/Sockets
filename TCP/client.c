b1588aa8b2c40058a21b38b676b00f1f7b318bbc
Radostin Stoyanov <rstoyanov1@gmail.com>
2019-09-27 21:01:32 +0100

    Improve TCP example
    Signed-off-by: Radostin Stoyanov <rstoyanov1@gmail.com>

--- TCP/client.c
+++ TCP/client.c
@@ -15,39 +15,41 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-	int sockfd, portno, n;
+	int sockfd, port, n;
 	struct sockaddr_in serv_addr;
 	struct hostent *server;
-
 	char buffer[256];
+
 	if (argc < 3) {
 		fprintf(stderr, "usage %s hostname port\n", argv[0]);
 		exit(0);
 	}
-	portno = atoi(argv[2]);
+
+	port = atoi(argv[2]);
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0)
 		error("ERROR opening socket");
+
 	server = gethostbyname(argv[1]);
-	if (server == NULL) {
+	if (!server) {
 		fprintf(stderr,"ERROR, no such host\n");
-		exit(0);
+		return -1;
 	}
+
 	bzero((char *) &serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
-	bcopy((char *)server->h_addr,
-			(char *)&serv_addr.sin_addr.s_addr,
-			server->h_length);
-	serv_addr.sin_port = htons(portno);
 
-	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+	serv_addr.sin_port = htons(port);
+
+	if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
 		error("ERROR connecting");
 
-	printf("Please enter the message: ");
 	bzero(buffer, 256);
+	printf("Please enter the message: ");
 	fgets(buffer, 255, stdin);
 
-	n = write(sockfd,buffer,strlen(buffer));
+	n = write(sockfd, buffer, strlen(buffer));
 	if (n < 0)
 		error("ERROR writing to socket");
 
